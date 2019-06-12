86de7042b8946f4de11439ad19a5d84081ad3b72
jkyony <jkyony@gmail.com>
2019-06-12 12:37:35 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<ion-app>
+  <router-view></router-view>
+</ion-app>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    // return {
+    //   msg: 'Welcome to Your Vue.js App'
+    // }
+  }
+}
+</script>
+
